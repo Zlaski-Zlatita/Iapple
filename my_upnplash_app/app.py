d7d9514778cfabed0e6bfa92ca5e44d43c262a90
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2025-02-05 12:33:36 +0500

    Create app.py

---
+++ my_upnplash_app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+UNSPLASH_API_KEY = 'YOUR_UNSPLASH_API_KEY'  # Замените на ваш API ключ Unsplash
+UNSPLASH_API_URL = 'https://api.unsplash.com/search/photos'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
+
+    response = requests.get(
+        UNSPLASH_API_URL,
+        params={
+            'query': query,
+            'page': page,
+            'client_id': UNSPLASH_API_KEY
+        }
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'Failed to fetch data'}), response.status_code
+
+if __name__ == '__main__':
+    app.run(debug=True)
